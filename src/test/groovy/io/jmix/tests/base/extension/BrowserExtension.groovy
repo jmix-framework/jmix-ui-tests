91e1a6a5999edf2f6e633bd7333dfefe19cf2a62
Gleb Durygin <durygin@haulmont.com>
2020-08-31 14:50:05 +0400

    Print url and password of VNC recorder in log

--- src/test/groovy/io/jmix/tests/base/extension/BrowserExtension.groovy
+++ src/test/groovy/io/jmix/tests/base/extension/BrowserExtension.groovy
@@ -6,18 +6,27 @@ import org.junit.jupiter.api.extension.AfterEachCallback
 import org.junit.jupiter.api.extension.BeforeEachCallback
 import org.junit.jupiter.api.extension.ExtensionContext
 import org.openqa.selenium.Capabilities
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.testcontainers.containers.BrowserWebDriverContainer
 
 abstract class BrowserExtension implements BeforeEachCallback, AfterEachCallback {
+
+    public static final int VNC_RECORDER_PORT = 5900
+
+    private static final Logger log = LoggerFactory.getLogger(BrowserExtension.class)
 
     private BrowserWebDriverContainer browser
 
     @Override
     void beforeEach(ExtensionContext context) throws Exception {
-        browser = new BrowserWebDriverContainer().withCapabilities(getCapabilities())
+        browser = new BrowserWebDriverContainer()
+                .withCapabilities(getCapabilities())
         browser.start()
         WebDriverRunner.setWebDriver(browser.getWebDriver())
         Selenide.open('/')
+
+        printVncRecordedUrl()
     }
 
     @Override
@@ -28,4 +37,12 @@ abstract class BrowserExtension implements BeforeEachCallback, AfterEachCallback
     }
 
     abstract Capabilities getCapabilities()
+
+    void printVncRecordedUrl() {
+        log.info('VNC recorder url: vnc://' +
+                browser.getContainerIpAddress() +
+                ':' +
+                browser.getMappedPort(VNC_RECORDER_PORT) +
+                ', password: \'secret\'')
+    }
 }
