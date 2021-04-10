c515cacb7e91c826777e6c49dd5e58f1068d9ca8
Gleb Durygin <durygin@haulmont.com>
2021-04-10 20:38:04 +0400

    Fix tests in ui package

--- src/test/groovy/io/jmix/tests/ui/BaseUiTest.groovy
+++ src/test/groovy/io/jmix/tests/ui/BaseUiTest.groovy
@@ -16,7 +16,7 @@ abstract class BaseUiTest {
      * Login as Administrator
      */
     static void loginAsAdmin() {
-        Selenide.open('/ui-tests')
+        Selenide.open('/')
         $j(LoginScreen).loginAsAdmin()
     }
 
