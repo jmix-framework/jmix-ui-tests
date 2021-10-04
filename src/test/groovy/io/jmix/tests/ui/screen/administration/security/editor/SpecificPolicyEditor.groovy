dbf476e4dbd7044cb7c3c17ccf35917e97b1dfae
Gleb Durygin <durygin@haulmont.com>
2021-10-04 10:36:47 +0400

    Fix failed tests

--- src/test/groovy/io/jmix/tests/ui/screen/administration/security/editor/SpecificPolicyEditor.groovy
+++ src/test/groovy/io/jmix/tests/ui/screen/administration/security/editor/SpecificPolicyEditor.groovy
@@ -3,13 +3,12 @@ package io.jmix.tests.ui.screen.administration.security.editor
 import io.jmix.masquerade.Wire
 import io.jmix.masquerade.base.Composite
 import io.jmix.masquerade.component.Button
-import io.jmix.masquerade.component.ComboBox
 import io.jmix.masquerade.component.TextField
 
 class SpecificPolicyEditor extends Composite<SpecificPolicyEditor> {
 
     @Wire
-    ComboBox resourceField
+    TextField resourceField
 
     @Wire
     TextField policyGroupField
